e74b10a1e397065500b9216ad90055a9828014cd
Will Dumm <wdumm88@gmail.com>
2022-12-18 22:43:55 -0800

    add files

---
+++ testlark_beast.py
@@ -0,0 +1,20 @@
+from lark import Lark, Transformer
+from tree_transformer import TreelistTransformer, NexusTransformer
+
+
+with open('newick.lark', 'r') as fh:
+    beast_translate_parser = Lark(fh, parser='lalr', start='beastfile', transformer=NexusTransformer())
+
+# with open('newick.lark.ebnf', 'r') as fh:
+#     newick_parser_inline = Lark(fh, parser='lalr', transformer=TreelistTransformer())
+#     # newick_parser = Lark(fh, debug=True)
+
+
+
+with open("clade_13.GTR.trees.nexus", 'r') as fh:
+    for line in fh:
+        if "Begin trees;" in line:
+            data = fh.read()
+
+trees = beast_translate_parser.parse(data)
+print(trees.pretty())
